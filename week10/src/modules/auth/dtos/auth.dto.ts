74b038b30e9d4052bc494e1d06436b5c4be8b75c
yeon-yeon1 <nojk0106@naver.com>
2026-05-28 17:38:34 +0900

    📝 Docs: 주석 해제 (#21)
    Docs: 주석 해제 (#21)

--- week10/src/modules/auth/dtos/auth.dto.ts
+++ week10/src/modules/auth/dtos/auth.dto.ts
@@ -1,20 +1,13 @@
 export interface SignupRequest {
-  /** 이메일 주소 */
   email: string;
-  /** 비밀번호 */
   password: string;
-  /** 닉네임 */
   nickname: string;
-  /** 전화번호 */
   phoneNum?: string;
-  /** 생일 (YYYY-MM-DD) */
   birthday?: string;
 }
 
 export interface LoginRequest {
-  /** 이메일 주소 */
   email: string;
-  /** 비밀번호 */
   password: string;
 }
 
